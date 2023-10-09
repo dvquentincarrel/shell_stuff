ad1b41c8b91c575766645d21ca92aa2d72a1a62a
dvquentincarrel <dvquentincarrel@gmail.com>
2023-10-09 13:02:07 +0200

    [FM] fzd properly filters out hidden dirs, '-a' option to see hidden dirs

--- aliases.sh
+++ aliases.sh
@@ -43,7 +43,6 @@ alias lg="ll -a --color=always | grep -i --color=never"
 alias functions="declare -F | cut -d' ' -f 3"
 alias mpcd='cd $(mktemp -d XXXXX)' # "Make Perishable cd"
 alias nbk='ln -nsf "$PWD" "$HOME/.config/nnn/bookmarks/000_TMP"' # Nnn BooKmark
-alias fzda='cd $(find . -type d -name .git -prune -o -type d -print | fzf)'
 alias tvim='vim -c "setl buftype=nofile"'
 
 # ===== Git =====  
