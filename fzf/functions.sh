9faaa18012e634f3c825bd3f8e4016d65cc72830
dvquentincarrel <dvquentincarrel@gmail.com>
2023-10-13 13:07:42 +0200

    Add alacritty stuff in its own dir, add makefile

---
+++ fzf/functions.sh
@@ -0,0 +1,12 @@
+# Fzf to a directory
+function fzd {
+    if [[ $1 == '-a' ]]; then
+        path=$(find . -type d -name .git -prune -o -type d -print | fzf --preview='tree -C {}')
+    else
+        path=$(find . -type d -path '*/.*' -prune -o -type d -print | fzf --preview='tree -C {}')
+    fi
+    if [[ -n $path ]]; then
+        cd "$path"
+    fi
+}
+export -f fzd
