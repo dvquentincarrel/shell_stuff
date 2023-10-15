35f304095178f99721561b847f617be9b87f0f0b
dvquentincarrel <dvquentincarrel@gmail.com>
2023-10-15 12:59:59 +0200

    [A] Handles window title for tmux

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -11,6 +11,8 @@ case ${TERM} in
 		PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\007"';;
 	screen*)
 		PROMPT_COMMAND='echo -ne "\033_${USER}@${HOSTNAME%%.*}:${PWD/#$HOME/\~}\033\\"';;
+    tmux*)
+		PROMPT_COMMAND='echo -ne "\033_ ${PWD/#$HOME/\~}\033\\"';;
 esac
 
 use_color=true
