05ea7378845b88c78b789dc089e65217435ef52a
dvquentincarrel <dvquentincarrel@gmail.com>
2023-10-20 09:42:10 +0200

    [F] Add include guard when sourcing qbl_init

--- bash/setup_prompt.sh
+++ bash/setup_prompt.sh
@@ -1,4 +1,6 @@
-which qbl_init >/dev/null && source qbl_init
+
+[[ $QBL_LOADED -ne '1' ]] && which qbl_init >/dev/null && source qbl_init
+QBL_LOADED=1
 if ! grep -q '\bshare_hist\b' <<< $PROMPT_COMMAND ; then
     PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND;} share_hist"
 fi
