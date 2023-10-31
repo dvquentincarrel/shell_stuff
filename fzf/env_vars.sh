d15f92cefac928d3cd3d0f991d4067a48f349ff1
dvquentincarrel <dvquentincarrel@gmail.com>
2023-10-31 19:56:59 +0100

    [M] Use FZF reverse layout

--- fzf/env_vars.sh
+++ fzf/env_vars.sh
@@ -4,4 +4,6 @@ export FZF_DEFAULT_OPTS='
     "ctrl-v:execute(vim {})","ctrl-d:execute(dragon {+})"
 --ansi
 --preview="$(which bat batcat false 2>/dev/null | head -n1) -p -r :50 --color=always {} || head -n 50 {}"
---multi'
+--multi
+--reverse
+'
