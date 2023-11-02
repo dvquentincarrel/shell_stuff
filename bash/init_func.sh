8f4dc9ec080cc00e21a5acf374cd031904588c98
dvquentincarrel <dvquentincarrel@gmail.com>
2023-11-02 13:08:28 +0100

    [A] feedback, to get visual feedback when something's done

--- bash/init_func.sh
+++ bash/init_func.sh
@@ -260,3 +260,30 @@ function gman {
     man -P "less -j1 '$command'" "$1"
 }
 export -f gman
+
+function feedback {
+	prev_val=$?
+	while getopts "h" opt; do
+		case $opt in
+			h)	echo "Show a unicolor pane in fullscreen, based on last command exit code or given hex color"
+				echo "Usage: feedback [HEX COLOR]"
+				return 0;;
+			*)	echo "invalid option '$opt'"
+					exit 1;;
+			esac
+		done
+		shift $((OPTIND -1))
+	if [[ -n $1 ]];then
+		color="#$1"
+	else
+		if [[ $prev_val -eq 0 ]];then
+			color="#00FF00"
+		else
+			color="#FF0000"
+		fi
+	fi
+	convert -size 16x9 xc:$color jpg:- | feh -F - >& /dev/null
+}
+export -f feedback
+
+# vim: ft=bash
