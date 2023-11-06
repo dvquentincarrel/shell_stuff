44a9f1b1a5ed292c57beee619cf7e0e4f7fdc23d
dvquentincarrel <dvquentincarrel@gmail.com>
2023-11-06 14:05:06 +0100

    [A] Scroll in fzf preview with ctrl-e / ctrl-y

--- fzf/env_vars.sh
+++ fzf/env_vars.sh
@@ -1,7 +1,7 @@
 export FZF_DEFAULT_OPTS='
 --height 50%
 --bind
-    "ctrl-v:execute(vim {})","ctrl-d:execute(dragon {+})"
+    "ctrl-v:execute(vim {})","ctrl-d:execute(dragon {+})","ctrl-e:preview-down","ctrl-y:preview-up"
 --ansi
 --preview="$(which bat batcat false 2>/dev/null | head -n1) -p -r :50 --color=always {} || head -n 50 {}"
 --multi
