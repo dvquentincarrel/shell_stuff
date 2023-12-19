a682fa88c94505debaecb31ce9595b97a813c71e
dvquentincarrel <dvquentincarrel@gmail.com>
2023-12-19 11:57:55 +0100

    [A] fzf: More visible hl colors

--- fzf/env_vars.sh
+++ fzf/env_vars.sh
@@ -1,9 +1,13 @@
 export FZF_DEFAULT_OPTS='
 --height 50%
---bind
-    "ctrl-v:execute($EDITOR {})","ctrl-d:execute(dragon {+})","ctrl-e:preview-down","ctrl-y:preview-up"
+--bind "ctrl-v:execute($EDITOR {})"
+--bind "ctrl-d:execute(dragon {+})"
+--bind "ctrl-e:preview-down"
+--bind "ctrl-y:preview-up"
+--bind "ctrl-/:change-preview-window(hidden|)"
 --ansi
---preview="$(which bat batcat false 2>/dev/null | head -n1) -p -r :50 --color=always {} || head -n 50 {}"
+--preview="$(which bat batcat false 2>/dev/null | head -n1) -p -r :50 --color=always {} 2>/dev/null || head -n 50 {} 2>/dev/null"
 --multi
 --reverse
+--color "hl:#FFAA00,hl+:#FFAA00"
 '
