70a45e0b694ef4ffbc8830a81341b14e1c040b51
dvquentincarrel <dvquentincarrel@gmail.com>
2024-02-26 13:27:29 +0100

    [M] fzd has variable depth, filters out hidden dirs

--- fzf/functions.sh
+++ fzf/functions.sh
@@ -1,12 +1,41 @@
 # Fzf to a directory
+
+function _fzd_getfiles {
+	if [[ $1 == '-a' ]]; then
+        local prune_type="-name"
+        local prune_target=".git"
+        shift
+	else
+        local prune_type="-path"
+        local prune_target="*/.*"
+	fi
+	local modmin=${1:-0}
+	local modmax=${2:-0}
+	local min=$((1 + modmin > 0 ? 1 + modmin : 1))
+	local max=$((5 + modmax > modmin ? 5 + modmax : modmin))
+    find . -mindepth $min -maxdepth $max -type d $prune_type "$prune_target" -prune -o -type d -print
+}
+export -f _fzd_getfiles
+
 function fzd {
-    if [[ $1 == '-a' ]]; then
-        path=$(find . -type d -name .git -prune -o -type d -print | fzf --preview='tree -C {}')
-    else
-        path=$(find . -type d -path '*/.*' -prune -o -type d -print | fzf --preview='tree -C {}')
+    local help_msg
+    read -d '' help_msg <<-EOF
+	Usage: fzd [-a] [MINMOD] [MAXMOD]
+	Use fzf to select a directory with a depth between 1 and 5, hiding hidden dirs by default.
+	Exemple: fzd -a 2 -1
+	Params:  -a        only hide .git dirs
+	         MINMOD    how much to add/subtract to the minimum depth
+	         MAXMOD    how much to add/subtract to the maximum depth
+	EOF
+    if [[ $1 == '-h' ]]; then
+        echo "$help_msg"
+        return 0
     fi
-    if [[ -n $path ]]; then
-        pushd "$path"
-    fi
+	local path=$(_fzd_getfiles $@ | \
+		fzf --preview='tree -C {}'
+	)
+	if [[ -n $path ]]; then
+		pushd "$path"
+	fi
 }
 export -f fzd
