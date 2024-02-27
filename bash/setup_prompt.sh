e25e9052f5774b09750acb33275f6a4bce8239ca
dvquentincarrel <dvquentincarrel@gmail.com>
2024-02-27 13:07:14 +0100

    [M] Remove qbl from shell init

--- bash/setup_prompt.sh
+++ bash/setup_prompt.sh
@@ -1,5 +1,3 @@
-[[ $QBL_LOADED -ne '1' ]] && which qbl_init >/dev/null && source qbl_init
-QBL_LOADED=1
 if ! grep -q '\bshare_hist\b' <<< $PROMPT_COMMAND ; then
     PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND;} share_hist"
 fi
