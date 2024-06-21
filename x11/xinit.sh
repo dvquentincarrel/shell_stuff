a8584a69de9584af3f37651a5816ab9655d9bc98
dvquentincarrel <dvquentincarrel@gmail.com>
2024-06-21 17:40:16 +0200

    [M] Change laptop primary screen

--- x11/xinit.sh
+++ x11/xinit.sh
@@ -2,7 +2,8 @@
 if uname -a | grep -qi manjaro; then
     xrandr --output HDMI-0 --primary --left-of DP-1
 elif uname -a | grep -qvi manjaro; then
-    xrandr --output HDMI-1 --left-of eDP-1 --primary
+    xrandr --output HDMI-1 --left-of eDP-1
+    xrandr --output eDP-1 --primary
 fi
 
 xset r rate 250 50 # delay, rate
