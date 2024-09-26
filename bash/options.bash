cba300614552c423e463be035aa7085d43bc58b7
dvquentincarrel <dvquentincarrel@gmail.com>
2024-09-26 13:20:50 +0200

    [A] bash: more options, reorder

--- bash/options.bash
+++ bash/options.bash
@@ -2,12 +2,14 @@ shopt -s cdspell # Attempts correction on small cd typos
 shopt -s checkjobs # Warns about background jobs before exiting
 shopt -s checkwinsize # Updates size after each command
 shopt -s cmdhist # Attempts to save multi-line commands in same hist entry
-shopt -s dirspell # Attempts correction on small dirname typos during autocomplete
 shopt -s direxpand # Expands vars when used like dirs
+shopt -s dirspell # Attempts correction on small dirname typos during autocomplete
 shopt -s expand_aliases # Expands aliases (necessary for aliases)
 shopt -s extglob # Extended glob patterns, regex-y
 shopt -s globstar # Enables **
 shopt -s histappend # Appends to history instead of overwriting
+shopt -s histreedit # Allows to retry failed history expansion
+shopt -s histverify # History-expanded commands are resent to readline buffer before execution
 shopt -s lithist # Multiline delimiters is \n instead of ; in hist
 shopt -s nocaseglob # Case-insensitive pathname globbing
 shopt -u huponexit # Sends SIGHUP to all jobs on shell exit
