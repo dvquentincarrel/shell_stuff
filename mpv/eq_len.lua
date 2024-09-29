24caa5354cae13a901b496864213c73fea7f77c3
dvquentincarrel <dvquentincarrel@gmail.com>
2024-09-29 12:54:09 +0200

    [M] mpv: Cleaner autoplay script

--- mpv/eq_len.lua
+++ mpv/eq_len.lua
@@ -15,11 +15,10 @@ mp.register_event("file-loaded", tfunc2)
 
 duration = mp.get_opt("dur")
 if(duration ~= nil) then
-	state = 1
+	active = true
 
 	function update_name()
-		if(state == 1)
-		then
+		if active then
 			mp.set_property("title", mp.get_property("media-title"))
 		else
 			mp.set_property("title", mp.get_property("media-title").." [Paused]")
@@ -32,33 +31,31 @@ if(duration ~= nil) then
 	end
 
 	function toggle_state()
-		if (state == -1) then -- if it IS paused
-			print("resumed")
+		if not active then -- if it IS paused
+			print("autoplay resumed")
 			timer:resume()
 			mp.osd_message("Resumed")
-		elseif (state == 1) then -- if it IS active
+		elseif active then -- if it IS active
 			timer:stop()
-			print("paused")
+			print("autoplay paused")
 			mp.osd_message("Paused")
 		end
-		state = -1 * state
+        active = not active
 		update_name()
 	end
 
 	function reset_timer()
 		timer:kill()
-		if (state == 1) then
+		if (active) then
 			timer:resume()
 		end	
 	end
 
-	print("duration: ",duration)
+	print("autplay delay: ",duration)
 	timer = mp.add_periodic_timer(duration, pl_next)
 
 	mp.add_key_binding("g", toggle_state)
 	mp.register_event("file-loaded", reset_timer)
 	mp.register_event("file-loaded", update_name)
-else
-	print("Inactive")
 end
 
