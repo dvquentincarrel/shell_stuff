028681ceeaa15690e2b868b1e1a47e9177645e41
dvquentincarrel <dvquentincarrel@gmail.com>
2024-09-29 12:53:45 +0200

    [MF] git: Cleaner and more functional autocompletion

--- git/autocomplete.bash
+++ git/autocomplete.bash
@@ -1,15 +1,11 @@
 function _gwr_complete {
-    local lines=$(git worktree list --porcelain | grep '^worktree' | awk '{print $2}' | tail -n +2)
-    IFS=$'\n'
-    COMREPLY=()
-    for line in $lines; do
-        COMPREPLY+=("$line")
-    done
+    local candidates=$(git worktree list --porcelain | awk '/^worktree/ && NR>1 {print $2}')
+    COMPREPLY=($(compgen -W "$candidates" -- $2))
 }
 complete -F _gwr_complete gwr
 
 function _gct_complete {
-    refs=$(git show-ref | cut -f 2 -d' ' | sed -e 's;refs/\w\+/;;')
+    local refs=$(git show-ref | awk '{sub(/refs\/\w+\/?/, "", $2); if ($2) print $2}')
     COMPREPLY=($(compgen -W "$refs" -- $2))
 }
 complete -F _gct_complete gct
