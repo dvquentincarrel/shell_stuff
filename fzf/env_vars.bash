63fdb61e411c23dc07e11a4343914984a2c723d4
Quentin Carrel <dvquentincarrel@gmail.com>
2024-10-23 23:35:32 +0200

    [M] fzf: add help, change some bindings

--- fzf/env_vars.bash
+++ fzf/env_vars.bash
@@ -1,10 +1,19 @@
+export FZF_HELP=\
+"[93m^E[m   open file with "'$EDITOR'"
+[93m^O[m   open file with xdg-open
+[93m^P[m   toggle preview
+[93m^?[m   show help
+[93m^↑[m   scroll preview up
+[93m^↓[m   scroll preview down
+[93mA-/[m  toggle line-wrap
+[93m^C / ^G / ^Q / Esc[m   exit"
 export FZF_DEFAULT_OPTS='
 --height 50%
 --bind "ctrl-e:execute($EDITOR {})"
 --bind "ctrl-d:execute(dragon {+})"
---bind "alt-down:preview-down"
---bind "alt-up:preview-up"
+--bind "ctrl-o:execute(xdg-open {})"
 --bind "ctrl-p:toggle-preview"
+--bind "ctrl-/:execute(fzf --height=100% --border-label=\"Help - Esc / ^C to leave\" --border=rounded --ansi --preview="" <<< $FZF_HELP)"
 --ansi
 --preview="fzf_preview.bash {}"
 --multi
