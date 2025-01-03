928d24b73e932ad58882561b51600d68722edfe4
Quentin Carrel <dvquentincarrel@gmail.com>
2025-01-03 16:26:19 +0100

    [M] Alacritty: colors follow ANSI, wrapper is simpler

--- alacritty/alacritty_wrapper.sh
+++ alacritty/alacritty_wrapper.sh
@@ -1,18 +1,1 @@
-if ! pgrep alacritty; then
-    export TMUX=
-    export ZELLIJ=
-    alacritty --hold --class HIDEME,HIDEME -e printf 'daemon window'&
-fi
-
-i=0
-# Posix sh loop
-while [ $i -lt 2000 ]; do
-    alacritty msg create-window "$@"
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    sleep 0.05
-    i=$((i + 1))
-done
-
-
+alacritty msg create-window "$@" 2>/dev/null || TMUX= ZELLIJ= alacritty --hold --class HIDEME,HIDEME -e alacritty msg create-window "$@"&
