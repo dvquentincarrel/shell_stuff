ac97d5440faf215068668573a807e84608592f53
Quentin Carrel <dvquentincarrel@gmail.com>
2025-01-16 17:31:07 +0100

    [MR] i3/x11: configs made modular

--- x11/xinit.sh
+++ x11/xinit.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-if uname -a | grep -qi manjaro; then
-    xrandr --output HDMI-0 --primary --left-of DP-1
-elif uname -a | grep -qvi manjaro; then
-    xrandr --output HDMI-1 --left-of eDP-1
-    xrandr --output eDP-1 --primary
-fi
+for file in "$XDG_CONFIG_HOME"/X11/*init.*sh; do . "$file"; done
 
 xset r rate 250 50 # delay, rate
 xset s off -dpms # Disable dpms, prevent screen from blanking
--- x11/xprofile.sh
+++ x11/xprofile.sh
@@ -3,13 +3,15 @@
 # Sourced by lightdm before .xsession
 
 if [ -f "$HOME/.profile" ]; then
-    source "$HOME/.profile"
+    . "$HOME/.profile"
 fi
 
 export WINIT_X11_SCALE_FACTOR=1 # Fixes issues with scaling on some software (Alacritty, Zoom, ...)
+export TERM_EMULATOR=alacritty_wrapper
+for file in $XDG_CONFIG_HOME/X11/*profile.*sh; do . "$file"; done
 
-if which xwallpaper; then
+if command -v xwallpaper; then
     export WALLPAPER_SETTER='xwallpaper --zoom'
-elif which feh; then
+elif command -v feh; then
     export WALLPAPER_SETTER='feh --bg-fill'
 fi
