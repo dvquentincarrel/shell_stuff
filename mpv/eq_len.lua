10362b403b593d695b4425cbdafcb968f767fb62
Quentin Carrel <dvquentincarrel@gmail.com>
2025-02-03 09:10:32 +0100

    [M] mpv: better eq_len code, full relative path in title

--- mpv/eq_len.lua
+++ mpv/eq_len.lua
@@ -1,61 +1,52 @@
 -- Automatically goes to next file after a certain duration
 
---[[
-require 'os'
-require 'io'
-require 'string'
-print("eq_len loaded")
-print("dur = " .. mp.get_opt("dur"))
-function tfunc2()
-	os.execute("sleep " .. mp.get_opt("dur"))
-	mp.command("playlist-next")
-end
-mp.register_event("file-loaded", tfunc2)
-]]--
-
 duration = mp.get_opt("dur")
 if(duration ~= nil) then
-	active = true
+    active = true
+    timer = nil
+    print(string.format("Autoplay delay: \x1b[96m%ss\x1b[m",duration))
 
-	function update_name()
-		if active then
-			mp.set_property("title", mp.get_property("media-title"))
-		else
-			mp.set_property("title", mp.get_property("media-title").." [Paused]")
-		end
-	end
+    function set_timer()
+        if(timer) then
+            timer:kill()
+        end
+        timer = mp.add_periodic_timer(duration, (function() mp.command("playlist-next") end))
+    end
+    set_timer()
 
-	function pl_next()
-		mp.command("playlist-next")
-		update_name()
-	end
+    function update_name()
+        title = string.format(
+            "%s (%ss) %s",
+            not active and "[Paused] " or "",
+            duration,
+            mp.get_property("media-title")
+        )
+        mp.set_property("title", title)
+    end
 
-	function toggle_state()
-		if not active then -- if it IS paused
-			print("autoplay resumed")
-			timer:resume()
-			mp.osd_message("Resumed")
-		elseif active then -- if it IS active
-			timer:stop()
-			print("autoplay paused")
-			mp.osd_message("Paused")
-		end
+    function toggle_pause()
+        reset_timer()
+        if not active then -- if it IS paused
+            print("\x1b[96mAutoplay resumed\x1b[m")
+            timer:resume()
+            mp.osd_message("Resumed")
+        elseif active then -- if it IS active
+            timer:stop()
+            print("\x1b[95mAutoplay paused\x1b[m")
+            mp.osd_message("Paused", 999999)
+        end
         active = not active
-		update_name()
-	end
+        update_name()
+    end
 
-	function reset_timer()
-		timer:kill()
-		if (active) then
-			timer:resume()
-		end	
-	end
+    function reset_timer()
+        timer:kill()
+        if (active) then
+            timer:resume()
+        end
+    end
 
-	print("autplay delay: ",duration)
-	timer = mp.add_periodic_timer(duration, pl_next)
-
-	mp.add_key_binding("g", toggle_state)
-	mp.register_event("file-loaded", reset_timer)
-	mp.register_event("file-loaded", update_name)
+    mp.add_key_binding("g", toggle_pause)
+    mp.register_event("file-loaded", reset_timer)
+    mp.register_event("file-loaded", update_name)
 end
-
