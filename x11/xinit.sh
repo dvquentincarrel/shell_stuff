2e18efdf1731b6a16af4de548ce926716135b81d
Quentin Carrel <dvquentincarrel@gmail.com>
2025-02-06 13:23:24 +0100

    [M] misc x11 stuff

--- x11/xinit.sh
+++ x11/xinit.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+source ~/.xprofile
 for file in "$XDG_CONFIG_HOME"/X11/*init.*sh; do . "$file"; done
 
 xset r rate 250 50 # delay, rate
 xset s off -dpms # Disable dpms, prevent screen from blanking
+xrandr --dpi 96
 setxkbmap leyaourt || setxkbmap 'us(altgr-intl)' || setxkbmap us
 
-exec i3
+dunst& </dev/null
+udiskie& </dev/null
+exec i3 </dev/null
