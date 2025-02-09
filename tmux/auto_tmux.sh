7bcbb6a50d0a77018aa5cbb1dfc2122f32844745
Quentin Carrel <dvquentincarrel@gmail.com>
2025-02-09 02:41:36 +0100

    [FM] tmux: quietly fails sourcing other* configs, silent auto_tmux checks

--- tmux/auto_tmux.sh
+++ tmux/auto_tmux.sh
@@ -1,7 +1,7 @@
 if which tmux &>/dev/null && [ -z "$TMUX" ] && [ -z "$NOMUXER" ] && [ $TERM != 'linux' ] && [ "$TERM_PROGRAM" != vscode ]; then
     # If enabled, joins the tmux session 'mgp' or creates it if it doesn't exist
     [ -f '/tmp/notmux' ] && return 0;
-    if tmux ls; then
+    if tmux ls &>/dev/null; then
         exec tmux new-session -t mgp
     else
         tmux new-session -d -s main -t mgp
