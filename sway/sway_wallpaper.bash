eec5c7defd1d70da1e5f84be86f6ac01222d4441
Quentin Carrel <dvquentincarrel@gmail.com>
2025-03-23 21:51:57 +0100

    [F] sway_wallpaper: fix bash's job control being quirky (outdated infos)

--- sway/sway_wallpaper.bash
+++ sway/sway_wallpaper.bash
@@ -3,7 +3,7 @@ pkill sway_wallpaper
 printf "%s" "${0##*/}" > /proc/$$/comm
 
 if [[ -z $1 ]]; then
-    msg="Usage: sway_wallpaper DELAY"
+    msg="Usage: sway_wallpaper DELAY (0 for non fixed backgrounds)"
     if tty &>/dev/null; then
         echo "$msg"
     else
@@ -22,29 +22,34 @@ delay=$(bc -l <<< "$1 / ${#outputs[@]}")
 function get_picture_list() {
     IFS= readarray -d '' files < <(find "$wallpaper_dir" -type f -print0 | shuf -z)
 }
-get_picture_list
+
+function set_bg() {
+    for output in ${outputs[@]}; do
+        pid=$(jobs -rp | head -n1)
+        swaybg -o $output -i "${files[-1]}" -m fill &>/dev/null &
+        if ! $SETUP; then
+            sleep 0.5 # Prevents flickering, 0.5 should be enough time for the new wallpaper to be set
+            kill $pid
+            wait $pid
+        fi
+        unset files[-1]
+        if [[ ${#files[@]} -eq 0 ]]; then
+            get_picture_list
+        fi
+    done
+}
 
 pids=$(pgrep swaybg)
-for output in ${outputs[@]}; do
-    echo  swaybg -o $output -i "${files[-1]}" -m fill
-    swaybg -o $output -i "${files[-1]}" -m fill &
-    unset files[-1]
-done
+SETUP=true
+get_picture_list
+set_bg
 kill $pids
+SETUP=false
 
 # Handle non-loop invokation
 [[ $1 = 0 ]] && exit
 
 while true; do
-    for output in ${outputs[@]}; do
-        pid=$(jobs -rp | head -n1)
-        swaybg -o $output -i "${files[-1]}" -m fill &
-        sleep 0.5 # Prevents flickering, 0.5 should be enough time for the new wallpaper to be set
-        kill $pid
-        unset files[-1]
-        if [[ ${#files[@]} -eq 0 ]]; then
-            get_picture_list
-        fi
-        sleep $delay
-    done
+    sleep $delay
+    set_bg
 done
