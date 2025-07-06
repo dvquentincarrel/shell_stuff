1933fdf16302c3af07be580aadea2fc686978187
Quentin Carrel <dvquentincarrel@gmail.com>
2025-07-06 20:27:30 +0200

    [A] fish: fzd/functions

--- fish/functions.fish
+++ fish/functions.fish
@@ -9,3 +9,20 @@ end
 function mcd -d "Make + cd"
     mkdir -p $argv[1] && cd $argv[1]
 end
+
+function fzd
+    set help_msg "Usage: fzd
+Use fzf & bf to select a directory"
+	if test "$argv[1]" = '-h'
+		echo "$help_msg"
+		return 0
+    end
+    set path $(
+		bf $argv | fzf --preview='tree -C {}'
+	)
+	if test -n "$path"
+		pushd "$path"
+    end
+end
+
+fzf --fish | source
